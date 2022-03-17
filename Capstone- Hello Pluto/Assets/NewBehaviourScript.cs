a69cff433dc36696b5d4888fc846e0aff62c6b8f
SadiaAshraf <97946577+SadiaAshraf@users.noreply.github.com>
2022-03-18 01:54:03 +0500

    In process

---
+++ Capstone- Hello Pluto/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float h;
+    float v;
+    public Vector2 speed = new Vector2(20, 20);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         h = Input.GetAxis("Horizontal");
+      
+        v= Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(speed.x * h, speed.y * v);
+        transform.Translate(movement * Time.deltaTime);
+    }
+}
