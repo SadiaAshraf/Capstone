cfddc1426b2b236e149c733dcb5c0b8bb278ce54
SadiaAshraf <97946577+SadiaAshraf@users.noreply.github.com>
2022-03-19 03:29:51 +0500

    In Process

---
+++ Capstone- Hello Pluto/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+    public Vector2 speed = new Vector2(20, 20);
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  transform.Translate(Vector2.up * Time.deltaTime * 10);
+        h = Input.GetAxis("Horizontal");
+
+        v = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(speed.x * h, speed.y * v);
+        transform.Translate(movement * Time.deltaTime);
+    }
+}
